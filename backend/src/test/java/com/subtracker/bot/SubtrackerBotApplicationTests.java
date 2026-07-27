27980df0d3e574966f801f1e14c6413fe55ed729
DhananjayaKularatne <djaya2100@gmail.com>
2026-07-27 17:37:46 +0530

    chore: scaffold backend structure and configure MySQL datasource

---
+++ backend/src/test/java/com/subtracker/bot/SubtrackerBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.subtracker.bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubtrackerBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
